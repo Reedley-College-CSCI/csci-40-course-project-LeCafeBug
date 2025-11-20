ec880c337ba6d7b39f2526ada5040a380796e4f9
LeCafeBug <edgarjarquin3@gmail.com>
2025-11-20 02:55:25 +0000

    test: Added a test for Recipe class

--- main.cpp
+++ main.cpp
@@ -13,14 +13,17 @@
 
 using namespace std;
 
+//test functions
 void testIngredientClass();
+void testRecipeClass();
 
 int main() {
     cout << "place holder name of program" << endl;
     cout << "-==±==-∞-===±∆±===-∞-==±==-" << endl;
     
+    //test functions
     testIngredientClass();
-
+    testRecipeClass();
     
 
     
--- testingBranch.cpp
+++ testingBranch.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Ingredients.h"
+#include "Recipe.h"
 
 using namespace std;
 
@@ -8,9 +9,9 @@ void testIngredientClass() {
     cout << "=±= Testing Ingredient Class =±=" << endl << endl;
     
     // Create test ingredient info
-    Ingredient flour("Flour", 9.99, 10.0, "Lbs");
-    Ingredient sugar("Sugar", 3.99, 4.0, "Oz");
-    Ingredient zeroTest("Test", 10.0, 0, "NA");
+    Ingredient flour( "Flour", 9.99, 10.0, "Lbs" );
+    Ingredient sugar( "Sugar", 3.99, 4.0, "Oz" );
+    Ingredient zeroTest( "Test", 10.0, 0, "NA" );
 
     // Test flour
     cout << "- - Testing Flour - -" << endl;
@@ -31,4 +32,30 @@ void testIngredientClass() {
     cout << "Cost per unit (0): $" << zeroTest.getCostPerUnit() << endl << endl;
 
     cout << "=≠= Testing for Ingredient class done =≠=" << endl;
+}
+
+void testRecipeClass() {
+    cout << "=±= Testing Recipe Class =±=" << endl << endl;
+
+    //Creates test recipe classes
+    Recipe cookies( "Cookies", 0.25, 1.30, 2, 12);
+    Recipe conchas( "Conchas", 0.40, 2.00, 4, 8);
+
+    //test cookies
+    cout << "- - Testing Cookies - -" << endl;
+    cout << "Name: " << cookies.getRecName() << endl;
+    cout << "Prep Time: " << cookies.getPrepTime() << " hours" << endl;
+    cout << "Bake Time: " << cookies.getBakeTime() << " hours" << endl;
+    cout << "Total Time: " << cookies.getTotalTime() << " hours" << endl;
+    cout << "Total: " << cookies.getBatchSize() << " batches of " << cookies.getYeildCount() << cookies.getRecName() << endl << endl;
+
+    //test conchas
+    cout << "- - Testing Conchas - -" << endl;
+    cout << "Name: " << conchas.getRecName() << endl;
+    cout << "Prep Time: " << conchas.getPrepTime() << " hours" << endl;
+    cout << "Bake Time: " << conchas.getBakeTime() << " hours" << endl;
+    cout << "Total Time: " << conchas.getTotalTime() << " hours" << endl;
+    cout << "Total: " << conchas.getBatchSize() << " batches of " << conchas.getYeildCount() << conchas.getRecName() << endl << endl;
+
+    cout << "=≠= Testing for Recipe class done =≠=" << endl;
 }
