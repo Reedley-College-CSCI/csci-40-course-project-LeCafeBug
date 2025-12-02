27a1abed42ecceaf3ed2186119c66ec5f60a08db
LeCafeBug <edgarjarquin3@gmail.com>
2025-12-02 02:21:09 +0000

    feat: Added the PQg and PP functions to the ingrediants class

--- Ingredients.cpp
+++ Ingredients.cpp
@@ -1,21 +1,29 @@
 #include "Ingredients.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 // getters
-const string Ingredient::getName() {
+const string Ingredient::getName() const{
     return InName;
     }
-const double Ingredient::getPrice() {
+const double Ingredient::getPrice() const{
     return price;
     }
-const double Ingredient::getQuantity() {
+const double Ingredient::getQuantity() const{
     return quantity;
     }
-const string Ingredient::getUnit() {
+const string Ingredient::getUnit() const{
     return unit;
     }
+
+const double Ingredient::getPQg() const{
+    return convertToGrams(quantity, unit);
+}
+const double Ingredient::getPP() const{
+    return price;
+}
 
 //constructors 
 Ingredient::Ingredient(){
@@ -39,4 +47,32 @@ const double Ingredient::getCostPerUnit() {
     }
     
     return price / quantity;
+}
+
+// Private helper function
+double Ingredient::convertToGrams(double q, const string fromUnit) const{
+    string unitLower = fromUnit;
+
+    for (char& c : unitLower) {
+        c = tolower(c);
+    }
+    
+    // Conversion to grams
+    if (unitLower == "g" || unitLower == "gram" || unitLower == "grams") {
+        return q;
+    } 
+    else if (unitLower == "oz" || unitLower == "ounce" || unitLower == "ounces") {
+        return q * 28.3495;
+    }
+    else if (unitLower == "lb" || unitLower == "lbs") {
+        return q * 453.592;
+    }
+    else if (unitLower == "count" || unitLower == "counts" ) {
+        return q;
+    }
+    else {
+        // Unknown unit - return original amount with warning
+        cout << "Warning: Unknown unit for ingredient '" << InName << endl;
+        return q;
+    }
 }
--- Ingredients.h
+++ Ingredients.h
@@ -16,23 +16,30 @@ class Ingredient {
 
 private:
     string InName;      // For the name of ingredients
-    double price;       
-    double quantity;      
-    string unit;        
+    double price;   
+    double quantity;
+    string unit;
+
+    double convertToGrams(double quantity, const string fromUnit) const;
 
 public:
     // Constructor to set object info
     Ingredient();
     Ingredient(string name, double price, double quantity, string unit);
 
-    const string getName();
-    const double getPrice();
-    const double getQuantity();
-    const string getUnit();
+    const string getName() const;
+    const double getPrice() const;
+    const double getQuantity() const;
+    const string getUnit() const;
+
+    const double getPQg()const;  // PQg means → Purchase Quantity in grams
+    const double getPP() const;   // PP means → Purchase Price 
 
     //might need a print function 
 
     const double getCostPerUnit(); 
+
+
 };
 
 #endif
